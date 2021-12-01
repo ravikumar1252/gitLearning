d60df465a18993189d27954b32ec55b5c38c40e4
RK <ravikumar1252@gmail.com>
2021-12-01 07:50:20 +0530

    using for Loop Data

--- src/main/java/com/slokam/SlokamStudentmgt/controller/TestController.java
+++ src/main/java/com/slokam/SlokamStudentmgt/controller/TestController.java
@@ -1,6 +1,12 @@
 package com.slokam.SlokamStudentmgt.controller;
 
 public class TestController {
-
-	//Enter Line 5 Test Data
+public static void main(String[] args) {
+	
+	for(int i=1;i<=5;i++) {
+		System.out.println("Hello");
+	}
+}
+	//Enter Line 5 Test Data 
+	
 }
